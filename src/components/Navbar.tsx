16d353685b2cb02c6c94fff9b1d3a146a7b6a691
poran-dip <porandip0@gmail.com>
2026-05-08 23:54:48 +0530

    feat: refine button ui primitive and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,21 @@
-import Logo from "../assets/logo.png";
-import Button from "./ui/Button";
+import { Link } from "react-router";
+import Logo from "@/assets/logo.png";
 
 const Navbar = () => {
-  const handleLogout = () => {
-    alert("Logged out!");
-  };
-
   return (
-    <div className="fixed top-0 left-0 right-0 w-screen h-12 sm:h-16 md:h-20 flex items-center justify-between py-1 md:py-2 px-1 sm:px-2 md:px-6">
-      <a href="/">
-        <div className="flex items-center gap-1 sm:gap-2 cursor-pointer hover:-translate-y-1 hover:scale-105 transition-all duration-200">
+    <div className="fixed top-0 left-0 right-0 w-full flex py-2 md:py-3 px-3 sm:px-4 md:px-6">
+      <Link to="/">
+        <div className="flex items-center gap-1 sm:gap-1.5 md:gap-2 cursor-pointer hover:-translate-y-1 transition-all duration-200">
           <img
             alt="Sky Notebook logo"
             src={Logo}
-            className="h-8 sm:h-12 md:h-16 w-8 sm:w-12 md:w-16"
+            className="h-8 sm:h-10 md:h-12 w-auto"
           />
-          <p className="text-lg sm:text-2xl md:text-3xl font-black text-blue-50">
+          <p className="text-lg sm:text-xl md:text-2xl font-black text-blue-50">
             SkyNotes
           </p>
         </div>
-      </a>
-
-      <div className="flex items-center pr-1 md:pr-2">
-        <Button onClick={handleLogout}>Log Out</Button>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,16 +1,39 @@
-import type { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children?: ReactNode;
-  className?: string;
+  size?: "sm" | "md";
+  variant?: "primary";
 }
 
-const Button = ({ onClick, children, className }: ButtonProps) => {
+const baseStyles = "font-medium transition-all duration-200 cursor-pointer";
+
+const buttonSizes = {
+  sm: "py-1 px-4 rounded-2xl text-sm",
+  md: "py-2 px-6 rounded-3xl",
+};
+
+const buttonVariants = {
+  primary:
+    "bg-blue-50 text-blue-400 shadow-md shadow-black/20 inset-shadow-[0_-2px_4px] inset-shadow-blue-900/50 hover:inset-shadow-[0_-4px_6px] hover:inset-shadow-blue-400/50"
+};
+
+const Button = ({
+  variant = "primary",
+  size = "md",
+  children,
+  className,
+  ...props
+}: ButtonProps) => {
   return (
     <button
       type="button"
-      className={`py-1 px-4 md:px-6 rounded-4xl text-sm sm:text-lg md:text-xl font-medium bg-blue-50 text-blue-400 shadow-md shadow-black/20 cursor-pointer inset-shadow-[0_-2px_4px] inset-shadow-blue-900/50 hover:inset-shadow-[0_-4px_6px] hover:inset-shadow-blue-400/50 transition-all duration-200 ${className}`}
-      onClick={onClick}
+      className={cn(
+        baseStyles,
+        buttonSizes[size],
+        buttonVariants[variant],
+        className,
+      )}
+      {...props}
     >
       {children}
     </button>
